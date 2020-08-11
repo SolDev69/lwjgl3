a3fda6c437cc98334247c2840bfea7dfa347622b
khanhduytran0 <khanhduytran032@gmail.com>
2020-08-11 10:10:59 +0700

    Try bridge cmake build

--- android_build/build_dyncall.bash
+++ android_build/build_dyncall.bash
@@ -1,12 +1,24 @@
 #!/bin/bash
 set -e
 cd dyncall-1.0
-rm -rf android_build
-mkdir android_build
-cd android_build
+rm -rf android_arm_build
+mkdir android_arm_build
+cd android_arm_build
 cmake .. \
 	-DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" \
-	-DANDROID_ABI=armeabi-v7a x86 \
+	-DANDROID_ABI=armeabi-v7a \
+	-DANDROID_NATIVE_API_LEVEL=21 \
+	-DBUILD_SHARED_LIBS=ON \
+	-DCMAKE_C_FLAGS="-no-integrated-as"
+
+cd ..
+
+rm -rf android_x86_build
+mkdir android_x86_build
+cd android_x86_build
+cmake .. \
+	-DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" \
+	-DANDROID_ABI=x86 \
 	-DANDROID_NATIVE_API_LEVEL=21 \
 	-DBUILD_SHARED_LIBS=ON \
 	-DCMAKE_C_FLAGS="-no-integrated-as"
