09a73d654df43b1b90176a88e828025660f6c9be
khanhduytran0 <khanhduytran032@gmail.com>
2020-08-22 11:21:15 +0700

    Make build script smaller

--- android_build/build_dyncall.bash
+++ android_build/build_dyncall.bash
@@ -5,46 +5,40 @@ cd dyncall-1.0
 # FIXME make it better, ex put to function
 # But it compile error!
 
+DEFAULT_CMAKE_ARGS= \
+	-DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" \
+	-DANDROID_NATIVE_API_LEVEL=21 \
+	-DBUILD_SHARED_LIBS=ON \
+	-DCMAKE_C_FLAGS="-no-integrated-as"
+
 rm -rf android_arm64_build
 mkdir android_arm64_build
 cd android_arm64_build
 cmake .. \
-	-DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" \
-	-DANDROID_ABI=arm64-v8a \
-	-DANDROID_NATIVE_API_LEVEL=21 \
-	-DBUILD_SHARED_LIBS=ON \
-	-DCMAKE_C_FLAGS="-no-integrated-as"
+	$DEFAULT_CMAKE_JEMALLOC_ARGS \
+	-DANDROID_ABI=arm64-v8a
+
+cd ..
+rm -rf android_arm_build
+mkdir android_arm_build
+cd android_arm_build
+cmake .. \
+	$DEFAULT_CMAKE_JEMALLOC_ARGS \
+	-DANDROID_ABI=armeabi-v7a
 
 cd ..
 rm -rf android_x86_64_build
 mkdir android_x86_64_build
 cd android_x86_64_build
 cmake .. \
-	-DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" \
-	-DANDROID_ABI=x86_64 \
-	-DANDROID_NATIVE_API_LEVEL=21 \
-	-DBUILD_SHARED_LIBS=ON \
-	-DCMAKE_C_FLAGS="-no-integrated-as"
+	$DEFAULT_CMAKE_JEMALLOC_ARGS \
+	-DANDROID_ABI=x86_64
 	
-cd ..
-rm -rf android_arm_build
-mkdir android_arm_build
-cd android_arm_build
-cmake .. \
-	-DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" \
-	-DANDROID_ABI=armeabi-v7a \
-	-DANDROID_NATIVE_API_LEVEL=21 \
-	-DBUILD_SHARED_LIBS=ON \
-	-DCMAKE_C_FLAGS="-no-integrated-as"
-
 cd ..
 rm -rf android_x86_build
 mkdir android_x86_build
 cd android_x86_build
 cmake .. \
-	-DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" \
-	-DANDROID_ABI=x86 \
-	-DANDROID_NATIVE_API_LEVEL=21 \
-	-DBUILD_SHARED_LIBS=ON \
-	-DCMAKE_C_FLAGS="-no-integrated-as"
+	$DEFAULT_CMAKE_JEMALLOC_ARGS \
+	-DANDROID_ABI=x86
 	
