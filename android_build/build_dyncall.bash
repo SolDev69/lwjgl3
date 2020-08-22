d9f4a4fe691c4f7b8688acff7e01c71ca5a644ef
khanhduytran0 <khanhduytran032@gmail.com>
2020-08-22 11:45:49 +0700

    Change env args to single line

--- android_build/build_dyncall.bash
+++ android_build/build_dyncall.bash
@@ -25,11 +25,7 @@ cd dyncall-1.0
 
 # FIXME make it better, ex put to function
 # But it compile error!
-DEFAULT_CMAKE_ARGS= \
-	-DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" \
-	-DANDROID_NATIVE_API_LEVEL=21 \
-	-DBUILD_SHARED_LIBS=ON \
-	-DCMAKE_C_FLAGS="-no-integrated-as"
+DEFAULT_CMAKE_ARGS="-DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" -DANDROID_NATIVE_API_LEVEL=21 -DBUILD_SHARED_LIBS=ON -DCMAKE_C_FLAGS=\"-no-integrated-as\""
 
 rm -rf android_arm64_build
 mkdir android_arm64_build
