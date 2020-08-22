aad18af004513961d56f926dcbd7fb709f9f12c5
khanhduytran0 <khanhduytran032@gmail.com>
2020-08-22 11:55:44 +0700

    Add backslashes

--- android_build/build_dyncall.bash
+++ android_build/build_dyncall.bash
@@ -1,31 +1,11 @@
 #!/bin/bash
 set -e
 
-func_build_and_cp () {
-	rm -rf android_$1_build
-	mkdir android_$1_build
-	cd android_$1_build
-	
-	cmake .. \
-		-DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" \
-		-DANDROID_ABI=$1 \
-		-DANDROID_NATIVE_API_LEVEL=21 \
-		-DBUILD_SHARED_LIBS=ON \
-		-DCMAKE_C_FLAGS="-no-integrated-as"
-	
-	make
-	
-	cp dyncall/libdyncall_s.a \
-		dyncallback/libdyncallback_s.a \
-		dynload/libdynload_s.a \
-		../../jni/$1/
-}
-
 cd dyncall-1.0
 
 # FIXME make it better, ex put to function
 # But it compile error!
-DEFAULT_CMAKE_ARGS="-DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" -DANDROID_NATIVE_API_LEVEL=21 -DBUILD_SHARED_LIBS=ON -DCMAKE_C_FLAGS=\"-no-integrated-as\""
+DEFAULT_CMAKE_ARGS="-DCMAKE_TOOLCHAIN_FILE=\"$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake\" -DANDROID_NATIVE_API_LEVEL=21 -DBUILD_SHARED_LIBS=ON -DCMAKE_C_FLAGS=\"-no-integrated-as\""
 
 rm -rf android_arm64_build
 mkdir android_arm64_build
