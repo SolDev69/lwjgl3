5876be7848f6b7b697312b709ab588c78b283b06
khanhduytran0 <khanhduytran032@gmail.com>
2020-08-23 08:21:25 +0700

    Replace 'nullptr' with 'NULL'

--- android_build/jni/binary_custom_dlopen.c
+++ android_build/jni/binary_custom_dlopen.c
@@ -8,7 +8,7 @@
 #include "binary_custom_dlopen.h"
 #include "log.h" // LOGE, LOGW, etc...
 
-bool (*_android_init_namespaces)(const char* public_ns_sonames, const char* anon_ns_library_path) = nullptr;
+bool (*_android_init_namespaces)(const char* public_ns_sonames, const char* anon_ns_library_path) = NULL;
 bool __unused android_init_namespaces(const char* public_ns_sonames, const char* anon_ns_library_path) {
 	if (!_android_init_namespaces) {
 		void *libdl_handle;
@@ -20,7 +20,7 @@ bool __unused android_init_namespaces(const char* public_ns_sonames, const char*
 		}
 		_android_init_namespaces = (bool (*)(const char*, const char*)) 
 									dlsym(libdl_handle, "android_init_namespaces");
-		if ((error = dlerror()) != nullptr)  {
+		if ((error = dlerror()) != NULL)  {
 			LOGE ("%s", error);
 			return false;
 		}
@@ -33,7 +33,7 @@ static struct android_namespace_t* (*_create_namespace)(const char* name,
                                               const char* default_library_path,
                                               uint64_t type,
                                               const char* permitted_when_isolated_path,
-                                              struct android_namespace_t* parent_namespace) = nullptr;
+                                              struct android_namespace_t* parent_namespace) = NULL;
 static struct android_namespace_t* create_namespace(const char* name,
                                               const char* ld_library_path,
                                               const char* default_library_path,
@@ -46,16 +46,16 @@ static struct android_namespace_t* create_namespace(const char* name,
 		libdl_handle = dlopen ("libdl.so", RTLD_NOW);
 		if (!libdl_handle) {
 			LOGE ("error opening libdl.so: %s", dlerror());
-			return nullptr;
+			return NULL;
 		}
 		_create_namespace = (struct android_namespace_t* (*)(const char*, const char*, const char*,
 								uint64_t, const char*, struct android_namespace_t* )) 
 									dlsym(libdl_handle, "android_create_namespace");
-		if (_create_namespace == nullptr)  {
-		    if ((error = dlerror()) != nullptr && strcmp(error, "undefined symbol") < 0)
+		if (_create_namespace == NULL)  {
+		    if ((error = dlerror()) != NULL && strcmp(error, "undefined symbol") < 0)
                  LOGE("error opening android_create_namespace in libdl.so: %s\n",  error);
 		    else LOGE("error opening android_create_namespace in libdl.so");
-			return nullptr;
+			return NULL;
 		}
 	}
 	LOGV("Creating namespace");
@@ -63,8 +63,8 @@ static struct android_namespace_t* create_namespace(const char* name,
 }
 
 // static char *ld_library_path = (char*)"/system/lib:/vendor/lib:/system/vendor/lib/hw/:/vendor/lib/hw";
-static struct android_namespace_t *ns = nullptr;
-static void* (*_dlopen_ext)(const char* filename, int flags, const android_dlextinfo* extinfo) = nullptr;
+static struct android_namespace_t *ns = NULL;
+static void* (*_dlopen_ext)(const char* filename, int flags, const android_dlextinfo* extinfo) = NULL;
 void* dlopen_ext(const char* ld_library_path, const char* filename, int flags) {
 	if (!_dlopen_ext) {
 		void *libdl_handle;
@@ -84,14 +84,14 @@ void* dlopen_ext(const char* ld_library_path, const char* filename, int flags) {
 		}
 	}
 
-	if (ns == nullptr) {
+	if (ns == NULL) {
 		// Not needed in JNI
 		// android_init_namespaces("", ld_library_path);
 		ns = create_namespace("system-namespace",ld_library_path, ld_library_path,0,ld_library_path,
-                              nullptr);
+                              NULL);
 	}
 
-	if (ns != nullptr && _dlopen_ext != nullptr) {
+	if (ns != NULL && _dlopen_ext != NULL) {
 		android_dlextinfo ext = {0};
 		ext.flags = ANDROID_DLEXT_USE_NAMESPACE;
 		ext.library_namespace = ns;
