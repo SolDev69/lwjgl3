9f46c028dbec5dc0108a3651acc25db71ffe8966
khanhduytran0 <khanhduytran032@gmail.com>
2020-08-23 17:54:48 +0700

    Changes

--- android_build/jni/binary_VMLauncher.c
+++ android_build/jni/binary_VMLauncher.c
@@ -101,10 +101,12 @@ static jint launchJVM(int argc, char** argv) {
        return 0;
    }
 
-        __android_log_print(3,"JVM", "Calling JLI_Launch");
-   return pJLI_Launch(argc, argv, 0, NULL, 0, NULL, FULL_VERSION,
-                          DOT_VERSION, *argv, *argv, JNI_FALSE, JNI_FALSE,
-                          JNI_FALSE, 0);
+   __android_log_print(3,"JVM", "Calling JLI_Launch");
+   return pJLI_Launch(argc, argv, 
+       0, NULL, 0, NULL,
+       FULL_VERSION,
+	   DOT_VERSION, "java", "openjdk", JNI_FALSE,
+	   JNI_TRUE, JNI_FALSE, 0);
 }
 
 /*
