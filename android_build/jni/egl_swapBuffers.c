ef11a276d232b25ac79f212705c72e72a05e04cb
khanhduytran0 <khanhduytran032@gmail.com>
2020-09-03 12:39:25 +0700

    Debug print EGL pointers

--- android_build/jni/egl_swapBuffers.c
+++ android_build/jni/egl_swapBuffers.c
@@ -6,34 +6,40 @@
 struct PotatoBridge {
 	void* eglContext;
 	void* eglDisplay;
-	void* eglReadSurface;
-	void* eglDrawSurface;
+	void* eglSurfaceRead;
+	void* eglSurfaceDraw;
 };
 struct PotatoBridge potatoBridge;
 
-JNIEXPORT void JNICALL Java_org_lwjgl_glfw_GLFW_setupEGL(JNIEnv* env, jclass clazz, jlong eglContext, jlong eglDisplay, jlong eglReadSurface, jlong eglDrawSurface) {
+JNIEXPORT void JNICALL Java_org_lwjgl_glfw_GLFW_setupEGL(JNIEnv* env, jclass clazz, jlong eglContext, jlong eglDisplay, jlong eglSurfaceRead, jlong eglSurfaceDraw) {
 	potatoBridge.eglContext = (intptr_t) eglContext;
 	potatoBridge.eglDisplay = (intptr_t) eglDisplay;
-	potatoBridge.eglReadSurface = (intptr_t) eglReadSurface;
-	potatoBridge.eglDrawSurface = (intptr_t) eglDrawSurface;
+	potatoBridge.eglSurfaceRead = (intptr_t) eglSurfaceRead;
+	potatoBridge.eglSurfaceDraw = (intptr_t) eglSurfaceDraw;
 }
 
 JNIEXPORT void JNICALL Java_net_kdt_pojavlaunch_BinaryExecutor_setupBridgeEGL(JNIEnv* env, jclass clazz) {
 	potatoBridge.eglContext = eglGetCurrentContext();
 	potatoBridge.eglDisplay = eglGetCurrentDisplay();
-	potatoBridge.eglReadSurface = eglGetCurrentSurface(EGL_READ);
-	potatoBridge.eglDrawSurface = eglGetCurrentSurface(EGL_DRAW);
+	potatoBridge.eglSurfaceRead = eglGetCurrentSurface(EGL_READ);
+	potatoBridge.eglSurfaceDraw = eglGetCurrentSurface(EGL_DRAW);
 }
 
 // Called from JNI_OnLoad of liblwjgl_opengl32
 void boardwalk2_openGLOnLoad() {
 	printf("EGL making current\n");
-	EGLBoolean success = eglMakeCurrent(potatoBridge.eglDisplay, potatoBridge.eglDrawSurface, potatoBridge.eglReadSurface, potatoBridge.eglContext);
+	printf("EGLContext=%p, EGLDisplay=%p, EGLSurfaceRead=%p, EGLSurfaceDraw=%p\n",
+		potatoBridge.eglContext,
+		potatoBridge.eglDisplay,
+		potatoBridge.eglSurfaceRead,
+		potatoBridge.eglSurfaceDraw 
+	);
+	EGLBoolean success = eglMakeCurrent(potatoBridge.eglDisplay, potatoBridge.eglSurfaceDraw, potatoBridge.eglSurfaceRead, potatoBridge.eglContext);
 	if (success == EGL_FALSE) printf("eglMakeCurrent() failed with error %p\n", eglGetError());
 }
 
 JNIEXPORT jboolean JNICALL Java_org_lwjgl_glfw_GLFW_nativeEglSwapBuffers(JNIEnv *env, jclass clazz) {
-	  return eglSwapBuffers(potatoBridge.eglDisplay, potatoBridge.eglDrawSurface);
+	  return eglSwapBuffers(potatoBridge.eglDisplay, potatoBridge.eglSurfaceDraw);
 }
 
 JNIEXPORT jboolean JNICALL Java_org_lwjgl_glfw_GLFW_nativeEglSwapInterval(JNIEnv *env, jclass clazz, jint interval) {
