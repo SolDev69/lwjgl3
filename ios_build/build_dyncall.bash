508207381bdc416f18ba079005bdf35d70f9e90f
dashboarder <73737593+dashboarder@users.noreply.github.com>
2020-12-25 20:22:00 +0700

    Update build_dyncall.bash

--- ios_build/build_dyncall.bash
+++ ios_build/build_dyncall.bash
@@ -3,36 +3,12 @@ set -e
 
 cd dyncall-1.0
 
+rm -rf ios_arm64_build
+mkdir ios_arm64_build
+cd ios_arm64_build
 
-chmod +x configure
-dobuild() {
-    export CC="$(xcrun -find -sdk ${SDK} cc)"
-    export CXX="$(xcrun -find -sdk ${SDK} cxx)"
-    export CPP="$(xcrun -find -sdk ${SDK} cpp)"
-    export CFLAGS="${HOST_FLAGS} ${OPT_FLAGS}"
-    export CXXFLAGS="${HOST_FLAGS} ${OPT_FLAGS}"
-    export LDFLAGS="${HOST_FLAGS}"
-
-    ./configure --host=${CHOST} --prefix=${PREFIX} --enable-static --disable-shared
-
-    make clean
-    make -j${MAKE_JOBS}
-    make install
-}
-
-SDK="iphoneos"
-ARCH_FLAGS="-arch arm64"
-HOST_FLAGS="${ARCH_FLAGS} -miphoneos-version-min=8.0 -isysroot $(xcrun -sdk ${SDK} --show-sdk-path)"
-CHOST="arm-apple-darwin"
-PREFIX="${HOME}/DEVICE_ARM64"
-dobuild
-
-# rm -rf ios_arm64_build
-# mkdir ios_arm64_build
-# cd ios_arm64_build
-
-# cmake -G Xcode -B build \
-#     -DCMAKE_SYSTEM_NAME=iOS \
-#     -DCMAKE_Swift_COMPILER_FORCED=true \
-#     -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 \
-#     -DCMAKE_C_FLAGS="-no-integrated-as" ..
+cmake -G Xcode -B build \
+    -DCMAKE_SYSTEM_NAME=iOS \
+    -DCMAKE_Swift_COMPILER_FORCED=true \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 \
+    -DCMAKE_C_FLAGS="-no-integrated-as -arch=arm64" ..
