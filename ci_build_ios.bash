6eb981aaaf1d18b6bb203aa21e22a931a313d3a8
khanhduytran0 <khanhduytran032@gmail.com>
2020-12-25 17:01:29 +0700

    ios

--- ci_build_ios.bash
+++ ci_build_ios.bash
@@ -3,16 +3,7 @@ set -e
 
 
 # wget http://beanshell.org/bsh-2.0b5.jar
-# mv bsh-2.0b5.jar $ANT_HOME/lib
-
-ant -version
-
-# Disable driftfx because some JDKs (eg OpenJDK on Ubuntu) don't come with JavaFX
-# Ignore ant build, since we are only building native code
- ANT_OPTS="-Dnashorn.args=\"--no-deprecation-warning\"" \
-
-ant -Dbinding.driftfx=false compile-templates compile release
-
+# mv bsh-2.0b5.jar $ANT_HO
 # ls *
 
 cd ios_build
@@ -49,6 +40,18 @@ cp dyncall-1.0/android_x86_build/dyncall/libdyncall_s.a \
 	dyncall-1.0/android_x86_build/dyncallback/libdyncallback_s.a \
 	dyncall-1.0/android_x86_build/dynload/libdynload_s.a \
 	jni/x86/
+
+cd ..
+
+ME/lib
+
+ant -version
+
+# Disable driftfx because some JDKs (eg OpenJDK on Ubuntu) don't come with JavaFX
+# Ignore ant build, since we are only building native code
+ ANT_OPTS="-Dnashorn.args=\"--no-deprecation-warning\"" \
+
+ant -Dplatform.ios=true -Dbinding.bullet=false -Dbinding.driftfx=false compile-templates compile release
 
 # Build LWJGL Android native libraries
 "$ANDROID_NDK_HOME/ndk-build"
