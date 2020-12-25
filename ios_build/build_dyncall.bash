f504be1c7ff87ec8a5c521413c501a46132e857f
dashboarder <73737593+dashboarder@users.noreply.github.com>
2020-12-25 20:19:48 +0700

    Update build_dyncall.bash

--- ios_build/build_dyncall.bash
+++ ios_build/build_dyncall.bash
@@ -5,7 +5,27 @@ cd dyncall-1.0
 
 
 chmod +x configure
-bash configure --prefix=ios_arm64_build --target=iOS
+dobuild() {
+    export CC="$(xcrun -find -sdk ${SDK} cc)"
+    export CXX="$(xcrun -find -sdk ${SDK} cxx)"
+    export CPP="$(xcrun -find -sdk ${SDK} cpp)"
+    export CFLAGS="${HOST_FLAGS} ${OPT_FLAGS}"
+    export CXXFLAGS="${HOST_FLAGS} ${OPT_FLAGS}"
+    export LDFLAGS="${HOST_FLAGS}"
+
+    ./configure --host=${CHOST} --prefix=${PREFIX} --enable-static --disable-shared
+
+    make clean
+    make -j${MAKE_JOBS}
+    make install
+}
+
+SDK="iphoneos"
+ARCH_FLAGS="-arch arm64"
+HOST_FLAGS="${ARCH_FLAGS} -miphoneos-version-min=8.0 -isysroot $(xcrun -sdk ${SDK} --show-sdk-path)"
+CHOST="arm-apple-darwin"
+PREFIX="${HOME}/DEVICE_ARM64"
+dobuild
 
 # rm -rf ios_arm64_build
 # mkdir ios_arm64_build
