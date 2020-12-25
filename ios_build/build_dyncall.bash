78fa9622b96f055978ed20562ba4f89942b906c8
dashboarder <73737593+dashboarder@users.noreply.github.com>
2020-12-25 20:49:55 +0700

    Update build_dyncall.bash

--- ios_build/build_dyncall.bash
+++ ios_build/build_dyncall.bash
@@ -15,3 +15,17 @@ cmake -G Xcode -B build \
     -DCMAKE_OSX_ARCHITECTURES=arm64 \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_C_FLAGS="-no-integrated-as" ..
+
+
+# FIXME make it better, ex put to function
+# But it compile error!
+(cd dyncall-1.0 && make)
+# (cd dyncall-1.0/android_arm_build/ && make)
+# (cd dyncall-1.0/android_x86_64_build/ && make)
+# (cd dyncall-1.0/android_x86_build/ && make)
+
+cp dyncall-1.0/ios_arm64_build/dyncall/libdyncall_s.a \
+	dyncall-1.0/ios_arm64_build/dyncallback/libdyncallback_s.a \
+	dyncall-1.0/ios_arm64_build/dynload/libdynload_s.a \
+	jni/arm64-v8a/
+	
