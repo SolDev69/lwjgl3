197085030a74e3d4556a4e3a86b0cf40279750b8
dashboarder <73737593+dashboarder@users.noreply.github.com>
2020-12-25 19:29:13 +0700

    Update build_dyncall.bash

--- ios_build/build_dyncall.bash
+++ ios_build/build_dyncall.bash
@@ -10,4 +10,4 @@ cmake -G Xcode -B build \
     -DCMAKE_SYSTEM_NAME=iOS \
     -DCMAKE_Swift_COMPILER_FORCED=true \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 \
-	-DCMAKE_C_FLAGS="-no-integrated-as"
+    -DCMAKE_C_FLAGS="-no-integrated-as" ..
