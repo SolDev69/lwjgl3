fd80ac63a74e855c4f5e505e31c06526faecba94
dashboarder <73737593+dashboarder@users.noreply.github.com>
2020-12-25 20:07:46 +0700

    Update build_dyncall.bash

--- ios_build/build_dyncall.bash
+++ ios_build/build_dyncall.bash
@@ -3,11 +3,16 @@ set -e
 
 cd dyncall-1.0
 
-rm -rf ios_arm64_build
-mkdir ios_arm64_build
-cd ios_arm64_build
-cmake -G Xcode -B build \
-    -DCMAKE_SYSTEM_NAME=iOS \
-    -DCMAKE_Swift_COMPILER_FORCED=true \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 \
-    -DCMAKE_C_FLAGS="-no-integrated-as" ..
+
+chmod +x configure
+bash configure --prefix=ios_arm64_build --target=iOS --sdk=11.0
+
+# rm -rf ios_arm64_build
+# mkdir ios_arm64_build
+# cd ios_arm64_build
+
+# cmake -G Xcode -B build \
+#     -DCMAKE_SYSTEM_NAME=iOS \
+#     -DCMAKE_Swift_COMPILER_FORCED=true \
+#     -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 \
+#     -DCMAKE_C_FLAGS="-no-integrated-as" ..
