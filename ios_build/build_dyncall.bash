b428743dab07785d423206b2143b08e5e9c42c80
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2020-12-26 11:27:38 +0700

    Update build_dyncall.bash

--- ios_build/build_dyncall.bash
+++ ios_build/build_dyncall.bash
@@ -10,17 +10,17 @@ cmake -G Xcode -B build \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 \
     -DCMAKE_OSX_ARCHITECTURES=arm64 \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_C_FLAGS="-no-integrated-as -arch arm64 -miphoneos-version-min=11.0" ..
+    -DCMAKE_C_FLAGS="-no-integrated-as -arch arm64 -miphoneos-version-min=11.0"
 
 # FIXME make it better, ex put to function
 # But it compile error!
-(cd dyncall-1.0 && make)
+make
 # (cd dyncall-1.0/android_arm_build/ && make)
 # (cd dyncall-1.0/android_x86_64_build/ && make)
 # (cd dyncall-1.0/android_x86_build/ && make)
 
-cp dyncall-1.0/ios_arm64_build/dyncall/libdyncall_s.a \
-	dyncall-1.0/ios_arm64_build/dyncallback/libdyncallback_s.a \
-	dyncall-1.0/ios_arm64_build/dynload/libdynload_s.a \
+cp dyncall/libdyncall_s.a \
+	dyncallback/libdyncallback_s.a \
+	dynload/libdynload_s.a \
 	jni/arm64-v8a/
 	
