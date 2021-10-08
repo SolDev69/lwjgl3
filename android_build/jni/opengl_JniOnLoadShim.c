261c35cace13d49ed7fd82cf6add304c49ed765d
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-10-08 09:19:08 +0700

    Update opengl_JniOnLoadShim.c

--- android_build/jni/opengl_JniOnLoadShim.c
+++ android_build/jni/opengl_JniOnLoadShim.c
@@ -7,12 +7,14 @@ void* pojavJni;
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
 	pojavJni = dlopen( /* "libboardwalk2_jni.so" */ "libpojavexec.so", RTLD_GLOBAL | RTLD_LAZY);
+	if (!pojavJni) return;
 	void (*pojav_openGLOnLoad)() = dlsym(pojavJni, "pojav_openGLOnLoad");
 	pojav_openGLOnLoad();
 	return JNI_VERSION_1_2;
 }
 
 JNIEXPORT void JNICALL JNI_OnUnload(JavaVM *vm, void *reserved) {
+	if (!pojavJni) return;
 	void (*pojav_openGLOnUnload)() = dlsym(pojavJni, "pojav_openGLOnUnload");
 	pojav_openGLOnUnload();
 	dlclose(pojavJni);
